f80735d46c51540784a76231e9b09cbc78ec87ef
Enkhbayar <enhuushaq@gmail.com>
2026-07-23 16:20:54 +0800

    fixed

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from '@prisma/config';
 import * as dotenv from 'dotenv';
 dotenv.config();
 export default defineConfig({
-  earlyAccess: true,
   datasource: {
     url: process.env.DATABASE_URL,
   },
